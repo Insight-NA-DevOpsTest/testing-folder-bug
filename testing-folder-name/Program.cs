b09af3034c54b4d70a422e0316c646bd5cdda8d7
Terraform User <brett.lewerke@insight.com>
2025-09-18 14:58:11 -0700

    Insight GitHub Repository Template

---
+++ testing-folder-name/Program.cs
@@ -0,0 +1,43 @@
+using testing_folder_name.Services;
+using testing_folder_name.Authentication;
+using testing_folder_name.Observability;
+using testing_folder_name.ErrorHandling;
+using testing_folder_name.Configuration;
+using testing_folder_name.Swagger;
+using Microsoft.OpenApi.Models;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// create basic console logging to provide visibility into configuration setup
+var logger = ObservabilityExtension.GetBasicConsoleLogger<Program>();
+builder.UseAzureAppConfiguration(logger);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerWithAuthentication(builder.Configuration.GetSection("Swagger"), logger);
+
+builder.Services.AddHttpClient();
+builder.AddDependencyChecks();
+builder.AddAuthentication(builder.Configuration.GetSection("Authentication"));
+
+// Add business services
+builder.Services.AddSingleton<IInsightService, InsightService>();
+builder.Services.Configure<InsightServiceConfig>(builder.Configuration.GetSection(InsightServiceConfig.Config));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwaggerWithOptions(builder.Configuration);
+}
+
+app.UseMiddleware<ExceptionMiddleware>();
+app.UseRouting();
+//Enable dev-certs and local HTTPS debugging first: ``app.UseHttpsRedirection();``
+app.UseAuthorization();
+app.MapControllers();
+app.MapHealthCheckEndpoint();
+
+await app.RunAsync();
